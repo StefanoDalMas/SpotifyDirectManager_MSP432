6d38c1d159eaeb38b4fe8356b66e8edba8d7d8dd
StefanoDalMas <stefano.dalmas01@gmail.com>
2022-12-09 13:52:52 +0100

    GET API test

--- src/ESP32Module/ESP32WiFi.ino/ESP32WiFi.ino.ino
+++ src/ESP32Module/ESP32WiFi.ino/ESP32WiFi.ino.ino
@@ -1,7 +1,17 @@
 #include "WiFi.h"
+#include "Arduino_JSON.h"
+#include "HTTPClient.h"
 
-const char* ssid = "iPhone di Stefano";
-const char* password = "oo8uki1diy6jy";
+
+const char* ssid = ""; //insert here
+const char* password = ""; //insert here
+const char* server = "https://cazza.free.beeceptor.com/getcazza";
+
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 5000; //5 sec
+
+                                                                                    
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,9 +22,9 @@ void setup() {
   WiFi.disconnect();
   delay(100);
 
+  scanWiFi(); //checks WiFi APs available
+  initWiFi(); //connect to desired AP
   Serial.println("Setup completed :)");
-
-
 }
 
 void scanWiFi(){
@@ -45,15 +55,48 @@ void initWiFi(){
     Serial.print('.');
     delay(1000);
   }
-  Serial.print("Connected to AP")
+  Serial.print("\n Connected to AP ");
   Serial.println(WiFi.localIP());  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  scanWiFi();
-  initWiFi();
-  while(){}
+  while(Serial.available()){
+    char c = Serial.read();
+    if (c == 's'){
+      Serial.println("Received start symbol");
+      getPage();
+    }
+  }
+}
+
+
+void getPage(){
+  if(WiFi.status()== WL_CONNECTED)
+  {
+    HTTPClient http;
+    http.begin(server);
+    Serial.println(server);
+    //Catch response
+    int response = http.GET();
+    Serial.println("HTTP code: ");
+    Serial.println(response);
+    if (response > 0){
+      if (response == HTTP_CODE_OK){ //It's 200
+          String val = http.getString();
+          Serial.println(val);
+      }
+    }
+    else{
+      Serial.println("HTTP Failed: error code is ");
+      Serial.println(http.errorToString(response).c_str()); //convert String into arryas of char with '\0' at the end
+    }
+
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+
 
 }
-  
