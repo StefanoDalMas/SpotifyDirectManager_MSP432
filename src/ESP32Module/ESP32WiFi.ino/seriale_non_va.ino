c86300a16980be2a5d0b0ba41dc66f070b2c9b36
StefanoDalMas <95777850+StefanoDalMas@users.noreply.github.com>
2023-02-17 18:35:54 +0100

    now reads strings from Serial2, added DEBUG mode

--- src/ESP32Module/ESP32WiFi.ino/seriale_non_va.ino
+++ src/ESP32Module/ESP32WiFi.ino/seriale_non_va.ino
@@ -5,15 +5,21 @@
 
 #define RX 16 //goes to 3.3
 #define TX 17 //goes to 3.2
+#define INTERNET_ACTIVE 0
+//WHACKY PROTOCOL
+//10-19 volume + playing
+//20-29 volume + not playing
+// % is 37, # is 35
 
 const char* ssid = "iPhone di Stefano"; //insert here
 const char* password = "oo8uki1diy6jy"; //insert here
-const char* token = "Bearer BQCiWAnOT_UxmkgudSofgRmDrh6L_dJHHRccSveqiii74KQHWT-4LwjOu9ItZJIlRwlImaGiw7wN6RIZM-Y6akn32_0j0FbS6A-YjpmMjefbDmIXFaDlzq3jyroMf0P6GL2ZrnnTGgT7FMSob8iWSK3hpIez4TDdHA9IfsUiVfBEsxcjE_ruGQTy7j_lOpX5mR1JbA"; //remember to create and add token
+const char* token = "Bearer BQCiVhkm3-T1jo3SY7Rqqk1JA_LM0U0UCZBSRoHC7WexC8xuOABw6m29KPtzXEMg7TGQbU-rfQ3w84QmxYxbZ5vREPvJOTqp7GBdUV_fIg9x0WmSgVOShjbWvi21FEeUufCieMmBRCNTFxMQgq6fT7OxQC4-owWzA1OipDW868oTtGyOY_qpJEDA9pxDY_vkMEpnyg"; //remember to create and add token
 const char* spotify_base_url = "https://api.spotify.com/v1/me";
 const int baud_rate = 115200;
 
 bool acked = false;
 int volume = 50;
+bool playing = false;
 char chr;
 char jsonOutput[128];
 String value;
@@ -30,36 +36,36 @@ void setup() {
   Serial.begin(baud_rate);
   Serial2.begin(baud_rate,SERIAL_8N1,RX,TX); //define serial channel
   //Disconnect from previous AP
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
-  scanWiFi(); //checks WiFi APs available
-  initWiFi(); //connect to desired AP
-  value = String("");
-  performHTTP("/player/volume?volume_percent=50",PUT);
+  if (INTERNET_ACTIVE){
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+    scanWiFi(); //checks WiFi APs available
+    initWiFi(); //connect to desired AP
+    value = String("");
+    performHTTP("/player/volume?volume_percent=50",PUT);
+    performHTTP("/player/pause",PUT);
+  }
   Serial.println("Setup completed :P");
   //Serial.println("Press g to perform GET, p to perform \"pause\" and n to perform \"next\" \n"); andy frocio
 }
 
 void loop() {
   while(Serial2.available()){
-      chr = Serial2.read();
-      if(chr >='a' && chr <='z'){
-        value.concat(chr);
-      }
-      if (strcmp(value.c_str(), "") != 0){
-        Serial.print("La stringa atm è ");
-        Serial.println(value);
-      }
-      
+    value = Serial2.readStringUntil('\0');
+    if (value.compareTo("") != 0){
       if (value.compareTo("prev") == 0){
-        performHTTP("/player/previous",POST);
+        if (INTERNET_ACTIVE){
+          performHTTP("/player/previous",POST); 
+        }
         Serial.println("HO VISTOOOOOO prev\n\n");
         value = String("");
         Serial2.write('%');
       }
       else if (value.compareTo("next") == 0){
-        performHTTP("/player/next",POST);
+        if (INTERNET_ACTIVE){
+          performHTTP("/player/next",POST);          
+        }
         Serial.println("HO VISTOOOOOO next\n\n");
         value = String("");
         Serial2.write('%');
@@ -69,7 +75,9 @@ void loop() {
           volume +=10;
           String set = "/player/volume?volume_percent=";
           set.concat(volume);
-          performHTTP(set,PUT);
+          if(INTERNET_ACTIVE){
+            performHTTP(set,PUT);
+          }          
         }
         Serial.println("HO VISTOOOOOO upup\n\n");
         value = String("");
@@ -80,27 +88,35 @@ void loop() {
           volume -= 10;
           String set = "/player/volume?volume_percent=";
           set.concat(volume);
-          performHTTP(set,PUT);
+          if (INTERNET_ACTIVE){
+            performHTTP(set,PUT);
+          }
         }
         Serial.println("HO VISTOOOOOO down\n\n");
         value = String("");
         Serial2.write('%');
       }
       else if (value.compareTo("play") == 0){
-        performHTTP("/player/play",PUT);
+        if (INTERNET_ACTIVE){
+          performHTTP("/player/play",PUT);
+        }
         Serial.println("HO VISTOOOOOO play\n\n");
         value = String("");
         Serial2.write('%');
       }
       else if (value.compareTo("stop") == 0){
+        if (INTERNET_ACTIVE){
         performHTTP("/player/pause",PUT);
+        }
         Serial.println("HO VISTOOOOOO stop\n\n");
         value = String(""); 
         Serial2.write('%');
       }  
     }
+  }
     value = String("");
 }
+
 
 
 void performHTTP(String url, mode m){
